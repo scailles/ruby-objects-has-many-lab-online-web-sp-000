c677d247ead097f8fdfbbbdd917282c246574344
Shaina C <scailles23@gmail.com>
2021-01-05 05:57:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,6 +21,10 @@ class Artist
     song.artist = self
     @@song_count += 1
   end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
 
   def self.song_count
     @@song_count
