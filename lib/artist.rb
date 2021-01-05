c79053b405de1da4d4c579ddc9f600f5f61b1aa5
Shaina C <scailles23@gmail.com>
2021-01-05 05:51:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,22 +11,22 @@ def initialize(name)
 end
 
 def songs
-  self.songs
+  @songs
 end
 
 def add_song(song)
-  self.songs << song
+  @songs << song
   song.artist = self
 end
 
 def add_song_by_name(song_name)
   song = Song.new(song_name)
   song.artist = self
-  self.songs << song
+  @songs << song
 end
 
 def self.song_count
-  self.songs.length
+  @songs.length
 end
 
 
