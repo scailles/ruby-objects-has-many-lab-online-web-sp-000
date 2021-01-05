7493e869dde97bf2b5d11991fd672bc0dfe133b9
Shaina C <scailles23@gmail.com>
2021-01-05 05:26:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,29 +3,30 @@ class Artist
   
 attr_accessor :name, :song
 
-@@songs = []
+
 
 def initialize(name)
   @name = name
+  @songs
 end
 
 def songs
-  @@songs
+  self.@songs
 end
 
 def add_song(song)
-  @@songs << song
+  self.@songs << song
   song.artist = self
 end
 
 def add_song_by_name(song_name)
   song = Song.new(song_name)
   song.artist = self
-  @@songs << song
+  self.@songs << song
 end
 
 def self.song_count
-  @@song_count.length
+  self.@songs.length
 end
 
 
